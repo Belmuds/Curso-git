f477db12de2b0fce314d4f36cc4462769bc2e845
Belmuds <belmuds.eng@gmail.com>
2020-02-02 17:42:54 -0300

    Acrescentou uma linha e retirou outra

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -6,9 +6,10 @@ namespace Projeto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tenha certeza que o dia hoje será muito bom!");
+            Console.WriteLine("");
         }
     }
 }
